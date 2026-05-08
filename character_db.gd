3245b536b147c0d79e64465058204bacda6bff9b
Ahmad Zaky <ahmadzakyy008@gmail.com>
2026-05-08 19:22:34 +0700

    Update character_db.gd

--- character_db.gd
+++ character_db.gd
@@ -1,9 +1,11 @@
 extends Node
-signal spawn_chara
+
+#sesuaikan lokasi scene yang anda gunakan ini hanya contoh
 
 var currrent_char : String
 var charDB = {
 	"player00" : "res://scene/player/player.tscn",
 	"player001" : "res://scene/player/player_001.tscn"
 }
+
 
