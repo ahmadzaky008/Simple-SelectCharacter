ecda34d60ec93c72bfbc6fe4522a3d2fce32067f
Ahmad Zaky <ahmadzakyy008@gmail.com>
2026-05-09 17:45:13 +0700

    Update spawn_location.gd

--- spawn_location.gd
+++ spawn_location.gd
@@ -9,5 +9,5 @@ func spawnChara():
 	var id = CharacterDB.currrent_char
 	var spawn = load(CharacterDB.charDB.get(id))
 	var character = spawn.instantiate()
-	character.position = $MarkerSpawn2D.position
+	character.pos = $MarkerSpawn2D.position
 	add_child(character)
